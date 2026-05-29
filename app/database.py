176404f24598ec9218b99630816b46a59e36615f
Eeshan Lom <eeshanlom@gmail.com>
2026-05-29 14:44:17 +0530

    Integrated PostgreSQL using environment variables

--- app/database.py
+++ app/database.py
@@ -1,10 +1,12 @@
+import os
+from dotenv import load_dotenv
 from sqlmodel import SQLModel, create_engine
 
-DATABASE_URL = "sqlite:///database.db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-
-
