9c0598179d5812173f1b9ea0c98881506466464f
Eeshan Lom <eeshanlom@gmail.com>
2026-06-03 11:19:41 +0530

    Add Alembic Database Migrations

--- app/main.py
+++ app/main.py
@@ -1,15 +1,8 @@
 from fastapi import FastAPI
-
-from app.database import create_db_and_tables
 from app.routes import registration
 
 
 app = FastAPI()
-
-
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
 
 
 @app.get("/")
